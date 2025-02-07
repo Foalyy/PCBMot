e488eb1eb878293248d7f80073e9fa2a3e450ac9
Anthony <a.gautier@silica.io>
2025-02-07 14:43:24 +0100

    Added command line arguments to specify config and output files

--- PCBMot.py
+++ PCBMot.py
@@ -3,28 +3,42 @@ from inkscape_drawing import InkscapeDrawing
 from config import Config, BoardShape, TerminalType
 from pcb import PCB
 from kicad import KicadPCB
+import argparse, sys
 
 
-# Config
-KICADPCB_FILENAME = 'examples/example.kicad_pcb'
-SVG_FILENAME = 'examples/example.svg'
-config = Config.read('examples/example_full.config')
+# Read arguments from the command line
+parser = argparse.ArgumentParser(
+    prog = 'PCBMot',
+    description = "Generate PCB motors easily",
+)
+parser.add_argument('-c', '--config', required=True, help="Config file that describes the design to generate")
+parser.add_argument('-o', '--output', help="SVG output file")
+parser.add_argument('-k', '--kicad', help="Kicad PCB output file")
+args = parser.parse_args()
+if args.output is None and args.kicad is None:
+    print("Please provide at least either the --output or the --kicad argument", file=sys.stderr)
+    sys.exit(1)
+
+# Read the config file
+config = Config.read(args.config)
 
 # Generate the PCB based on the given config
 pcb = PCB.generate(config)
 
 # Create the SVG drawing
-dwg = InkscapeDrawing(
-    SVG_FILENAME,
-    size = (f"{int(config.viewport_width * config.svg_scale)}px", f"{int(config.viewport_height * config.svg_scale)}px"),
-    coords = (-config.viewport_width/2.0, -config.viewport_height/2.0, config.viewport_width, config.viewport_height),
-    profile = config.svg_profile,
-    background_color = config.background_color,
-)
-pcb.draw_svg(dwg)
-dwg.save()
+if args.output:
+    dwg = InkscapeDrawing(
+        args.output,
+        size = (f"{int(config.viewport_width * config.svg_scale)}px", f"{int(config.viewport_height * config.svg_scale)}px"),
+        coords = (-config.viewport_width/2.0, -config.viewport_height/2.0, config.viewport_width, config.viewport_height),
+        profile = config.svg_profile,
+        background_color = config.background_color,
+    )
+    pcb.draw_svg(dwg)
+    dwg.save()
 
 # Create the Kicad board
-kicadpcb = KicadPCB(KICADPCB_FILENAME, config)
-pcb.draw_kicad(kicadpcb)
-kicadpcb.save()
+if args.kicad:
+    kicadpcb = KicadPCB(args.kicad, config)
+    pcb.draw_kicad(kicadpcb)
+    kicadpcb.save()
